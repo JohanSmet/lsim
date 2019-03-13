e07ff9ee95fa327fa050843e9300326e3f407260
Johan Smet <johan.smet@justcode.be>
2019-03-13 22:57:36 +0100

    Try to use the 'override' keyword were appropriate

--- src/gate.h
+++ src/gate.h
@@ -19,13 +19,13 @@ private:
 class Buffer : public CloneComponent<Buffer> {
 public:
     Buffer(size_t data_bits);
-    virtual void process();
+    virtual void process() override;
 };
 
 class TriStateBuffer : public CloneComponent<TriStateBuffer> {
 public:
     TriStateBuffer(size_t data_bits);
-    virtual void process();
+    virtual void process() override;
     pin_t enable_pin() const {return m_pins[m_enable_idx];}
 private:
     size_t  m_enable_idx;
@@ -34,43 +34,43 @@ private:
 class AndGate : public CloneComponent<AndGate> {
 public:
     AndGate(size_t num_inputs);
-    virtual void process();
+    virtual void process() override;
 };
 
 class OrGate : public CloneComponent<OrGate> {
 public:
     OrGate(size_t num_inputs);
-    virtual void process();
+    virtual void process() override;
 };
 
 class NotGate : public CloneComponent<NotGate> {
 public:
     NotGate();
-    virtual void process();
+    virtual void process() override;
 };
 
 class NandGate : public CloneComponent<NandGate> {
 public:
     NandGate(size_t num_inputs);
-    virtual void process();
+    virtual void process() override;
 };
 
 class NorGate : public CloneComponent<NorGate> {
 public:
     NorGate(size_t num_inputs);
-    virtual void process();
+    virtual void process() override;
 };
 
 class XorGate : public CloneComponent<XorGate> {
 public:
     XorGate();
-    virtual void process();
+    virtual void process() override;
 };
 
 class XnorGate : public CloneComponent<XnorGate> {
 public:
     XnorGate();
-    virtual void process();
+    virtual void process() override;
 };
 
 #endif // LSIM_GATE_H
