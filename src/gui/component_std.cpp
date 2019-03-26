8fe823ed8984e848137a57e5380fed72099e6ca3
Johan Smet <johan.smet@justcode.be>
2019-03-26 16:03:20 +0100

    GUI: support for all basic gates

--- src/gui/component_std.cpp
+++ src/gui/component_std.cpp
@@ -69,4 +69,91 @@ void component_register_gates() {
         }
     );
 
+    // tristate buffer
+	ComponentIcon icon_tristate_buffer(SHAPE_TRISTATE_BUFFER, sizeof(SHAPE_TRISTATE_BUFFER));
+    UICircuitBuilder::register_materialize_func(
+        VisualComponent::TRISTATE_BUFFER, [=](Component *comp, UIComponent *ui_comp, UICircuit *ui_circuit) {
+            ui_comp->m_tooltip = "Tri-state Buffer";
+            ui_comp->m_icon = &icon_tristate_buffer;
+            int bits = (comp->num_pins() - 1) / 2;
+            materialize_gate(ui_comp, ui_circuit, comp->pins(0, bits-1), comp->pins(bits+1, bits*2), comp->pins(bits, bits));
+        }
+    );
+
+    // AND gate
+	ComponentIcon icon_and(SHAPE_AND_GATE, sizeof(SHAPE_AND_GATE));
+    UICircuitBuilder::register_materialize_func(
+        VisualComponent::AND_GATE, [=](Component *comp, UIComponent *ui_comp, UICircuit *ui_circuit) {
+            ui_comp->m_tooltip = "AND";
+            ui_comp->m_icon = &icon_and;
+            int inputs = comp->num_pins() - 1;
+            materialize_gate(ui_comp, ui_circuit, comp->pins(0, inputs-1), comp->pins(inputs, inputs), {});
+        }
+    );
+
+    // OR gate
+	ComponentIcon icon_or(SHAPE_OR_GATE, sizeof(SHAPE_OR_GATE));
+    UICircuitBuilder::register_materialize_func(
+        VisualComponent::OR_GATE, [=](Component *comp, UIComponent *ui_comp, UICircuit *ui_circuit) {
+            ui_comp->m_tooltip = "OR";
+            ui_comp->m_icon = &icon_or;
+            int inputs = comp->num_pins() - 1;
+            materialize_gate(ui_comp, ui_circuit, comp->pins(0, inputs-1), comp->pins(inputs, inputs), {});
+        }
+    );
+
+    // NOT gate
+	ComponentIcon icon_not(SHAPE_NOT_GATE, sizeof(SHAPE_NOT_GATE));
+    UICircuitBuilder::register_materialize_func(
+        VisualComponent::NOT_GATE, [=](Component *comp, UIComponent *ui_comp, UICircuit *ui_circuit) {
+            ui_comp->m_tooltip = "NOT";
+            ui_comp->m_icon = &icon_not;
+            int inputs = comp->num_pins() - 1;
+            materialize_gate(ui_comp, ui_circuit, comp->pins(0, inputs-1), comp->pins(inputs, inputs), {});
+        }
+    );
+
+    // NAND gate
+	ComponentIcon icon_nand(SHAPE_NAND_GATE, sizeof(SHAPE_NAND_GATE));
+    UICircuitBuilder::register_materialize_func(
+        VisualComponent::NAND_GATE, [=](Component *comp, UIComponent *ui_comp, UICircuit *ui_circuit) {
+            ui_comp->m_tooltip = "NAND";
+            ui_comp->m_icon = &icon_nand;
+            int inputs = comp->num_pins() - 1;
+            materialize_gate(ui_comp, ui_circuit, comp->pins(0, inputs-1), comp->pins(inputs, inputs), {});
+        }
+    );
+
+    // NOR gate
+	ComponentIcon icon_nor(SHAPE_NOR_GATE, sizeof(SHAPE_NOR_GATE));
+    UICircuitBuilder::register_materialize_func(
+        VisualComponent::NOR_GATE, [=](Component *comp, UIComponent *ui_comp, UICircuit *ui_circuit) {
+            ui_comp->m_tooltip = "NOR";
+            ui_comp->m_icon = &icon_nor;
+            int inputs = comp->num_pins() - 1;
+            materialize_gate(ui_comp, ui_circuit, comp->pins(0, inputs-1), comp->pins(inputs, inputs), {});
+        }
+    );
+
+    // XOR gate
+	ComponentIcon icon_xor(SHAPE_XOR_GATE, sizeof(SHAPE_XOR_GATE));
+    UICircuitBuilder::register_materialize_func(
+        VisualComponent::XOR_GATE, [=](Component *comp, UIComponent *ui_comp, UICircuit *ui_circuit) {
+            ui_comp->m_tooltip = "XOR";
+            ui_comp->m_icon = &icon_xor;
+            int inputs = comp->num_pins() - 1;
+            materialize_gate(ui_comp, ui_circuit, comp->pins(0, inputs-1), comp->pins(inputs, inputs), {});
+        }
+    );
+
+    // XNOR gate
+	ComponentIcon icon_xnor(SHAPE_XNOR_GATE, sizeof(SHAPE_XNOR_GATE));
+    UICircuitBuilder::register_materialize_func(
+        VisualComponent::XNOR_GATE, [=](Component *comp, UIComponent *ui_comp, UICircuit *ui_circuit) {
+            ui_comp->m_tooltip = "XNOR";
+            ui_comp->m_icon = &icon_xnor;
+            int inputs = comp->num_pins() - 1;
+            materialize_gate(ui_comp, ui_circuit, comp->pins(0, inputs-1), comp->pins(inputs, inputs), {});
+        }
+    );
 }
