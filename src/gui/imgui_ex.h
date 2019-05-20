409784f6514face252e7399ae2ab5985e3996f00
Johan Smet <johan.smet@justcode.be>
2019-05-20 21:22:15 +0200

    GUI: Add ImGuiEx functions to draw rotated text

--- src/gui/imgui_ex.h
+++ src/gui/imgui_ex.h
@@ -3,6 +3,7 @@
 // QOL extensions for imgui
 
 #include "imgui/imgui.h"
+#include "algebra.h"
 
 namespace ImGuiEx {
 
@@ -19,9 +20,17 @@ enum TextAlignVer {
 };
 
 void Text(ImVec2 at, const char *text, TextAlignHor align_hor = TAH_LEFT, TextAlignVer align_ver = TAV_TOP);
+void TextNoClip(ImVec2 at, const char *text, ImU32 col, TextAlignHor align_hor = TAH_LEFT, TextAlignVer align_ver = TAV_TOP);
 inline void Text(ImVec2 at, const std::string &text, TextAlignHor align_hor = TAH_LEFT, TextAlignVer align_ver = TAV_TOP) {
     Text(at, text.c_str(), align_hor, align_ver);
 }
+inline void TextNoClip(ImVec2 at, const std::string &text, ImU32 col, TextAlignHor align_hor = TAH_LEFT, TextAlignVer align_ver = TAV_TOP) {
+    TextNoClip(at, text.c_str(), col, align_hor, align_ver);
+}
 void RectFilled(ImVec2 p1, ImVec2 p2, ImU32 col);
+
+void TransformStart();
+void TransformEnd(Transform transform);
+
 
 } // namespace ImGuiEx
