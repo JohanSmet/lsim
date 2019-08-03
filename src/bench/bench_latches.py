a51c47b3970214e475d47ab973829e40ddcc5621
Johan Smet <johan.smet@justcode.be>
2019-08-03 16:52:13 +0200

    lib_latches.lsim: add 8bit D FlipFlop (+tristated version)

--- src/bench/bench_latches.py
+++ src/bench/bench_latches.py
@@ -7,6 +7,7 @@ lsim = lsimpy.LSimContext()
 
 HIGH = lsimpy.ValueTrue
 LOW = lsimpy.ValueFalse
+NONE = lsimpy.ValueUndefined
 
 def test_sr_latch():
     truth_table = [
@@ -222,6 +223,51 @@ def test_masterslave_d_flipflop():
 
     run_thruth_table(lsim, "MasterSlave D FlipFlop", truth_table)
 
+def test_async_d_flipflop_8bit():
+    truth_table = [
+        [{'Clk': LOW, 'Res': LOW,  'Pre': HIGH, 'D[0]': LOW, 'D[1]': LOW, 'D[2]': LOW, 'D[3]': LOW, 'D[4]': LOW, 'D[5]': LOW, 'D[6]': LOW, 'D[7]': LOW }, { 'Q[0]': HIGH, 'Q[1]': HIGH, 'Q[2]': HIGH, 'Q[3]': HIGH, 'Q[4]': HIGH, 'Q[5]': HIGH, 'Q[6]': HIGH, 'Q[7]': HIGH}],
+        [{'Clk': LOW, 'Res': HIGH, 'Pre': LOW,  'D[0]': LOW, 'D[1]': LOW, 'D[2]': LOW, 'D[3]': LOW, 'D[4]': LOW, 'D[5]': LOW, 'D[6]': LOW, 'D[7]': LOW }, { 'Q[0]': LOW,  'Q[1]': LOW,  'Q[2]': LOW,  'Q[3]': LOW,  'Q[4]': LOW,  'Q[5]': LOW,  'Q[6]': LOW,  'Q[7]': LOW}]
+    ]
+
+    for v in range(256):
+        inputs = {f"D[{i:}]" : HIGH if ((v >> i) & 1) == 1 else LOW for i in range(8)}
+        inputs.update([('Clk', HIGH), ('Res', LOW), ('Pre', LOW)])
+
+        outputs = {f"Q[{i:}]" : HIGH if ((v >> i) & 1) == 1 else LOW for i in range(8)}
+        truth_table.append([inputs, outputs])
+
+        inputs = {f"D[{i:}]" : HIGH if ((v >> i) & 1) == 1 else LOW for i in range(8)}
+        inputs.update([('Clk', LOW), ('Res', LOW), ('Pre', LOW)])
+        truth_table.append([inputs, outputs])
+
+    run_thruth_table(lsim, "Async D FlipFlop 8b", truth_table)
+
+def test_async_d_flipflop_8bit_tristate():
+    truth_table = [
+        [{'Clk': LOW, 'Res': LOW,  'Pre': HIGH, 'OE': HIGH, 'D[0]': LOW, 'D[1]': LOW, 'D[2]': LOW, 'D[3]': LOW, 'D[4]': LOW, 'D[5]': LOW, 'D[6]': LOW, 'D[7]': LOW }, { 'Q[0]': HIGH, 'Q[1]': HIGH, 'Q[2]': HIGH, 'Q[3]': HIGH, 'Q[4]': HIGH, 'Q[5]': HIGH, 'Q[6]': HIGH, 'Q[7]': HIGH}],
+        [{'Clk': LOW, 'Res': LOW,  'Pre': HIGH, 'OE': LOW,  'D[0]': LOW, 'D[1]': LOW, 'D[2]': LOW, 'D[3]': LOW, 'D[4]': LOW, 'D[5]': LOW, 'D[6]': LOW, 'D[7]': LOW }, { 'Q[0]': NONE, 'Q[1]': NONE, 'Q[2]': NONE, 'Q[3]': NONE, 'Q[4]': NONE, 'Q[5]': NONE, 'Q[6]': NONE, 'Q[7]': NONE}],
+        [{'Clk': LOW, 'Res': HIGH, 'Pre': LOW,  'OE': HIGH, 'D[0]': LOW, 'D[1]': LOW, 'D[2]': LOW, 'D[3]': LOW, 'D[4]': LOW, 'D[5]': LOW, 'D[6]': LOW, 'D[7]': LOW }, { 'Q[0]': LOW,  'Q[1]': LOW,  'Q[2]': LOW,  'Q[3]': LOW,  'Q[4]': LOW,  'Q[5]': LOW,  'Q[6]': LOW,  'Q[7]': LOW}],
+        [{'Clk': LOW, 'Res': HIGH, 'Pre': LOW,  'OE': LOW,  'D[0]': LOW, 'D[1]': LOW, 'D[2]': LOW, 'D[3]': LOW, 'D[4]': LOW, 'D[5]': LOW, 'D[6]': LOW, 'D[7]': LOW }, { 'Q[0]': NONE, 'Q[1]': NONE, 'Q[2]': NONE, 'Q[3]': NONE, 'Q[4]': NONE, 'Q[5]': NONE, 'Q[6]': NONE, 'Q[7]': NONE}]
+    ]
+
+    outputs_none = {f"Q[{i:}]" : NONE for i in range(8)}
+
+    for v in range(256):
+        inputs = {f"D[{i:}]" : HIGH if ((v >> i) & 1) == 1 else LOW for i in range(8)}
+        inputs.update([('Clk', HIGH), ('Res', LOW), ('Pre', LOW), ('OE', LOW)])
+        truth_table.append([inputs, outputs_none])
+
+        inputs = {f"D[{i:}]" : HIGH if ((v >> i) & 1) == 1 else LOW for i in range(8)}
+        inputs.update([('Clk', LOW), ('Res', LOW), ('Pre', LOW), ('OE', LOW)])
+        truth_table.append([inputs, outputs_none])
+
+        inputs = {f"D[{i:}]" : HIGH if ((v >> i) & 1) == 1 else LOW for i in range(8)}
+        inputs.update([('Clk', LOW), ('Res', LOW), ('Pre', LOW), ('OE', HIGH)])
+        outputs = {f"Q[{i:}]" : HIGH if ((v >> i) & 1) == 1 else LOW for i in range(8)}
+        truth_table.append([inputs, outputs])
+
+    run_thruth_table(lsim, "Async D FlipFlop 8bt", truth_table)
+
 def main():
     if (not lsim.load_user_library("../../examples/cpu_8bit/lib_latches.lsim")):
         print("Unable to load circuit\n")
@@ -238,6 +284,8 @@ def main():
     test_async_d_flipflop()
     test_async_jk_flipflop()
     test_masterslave_d_flipflop()
+    test_async_d_flipflop_8bit()
+    test_async_d_flipflop_8bit_tristate()
 
     print_stats()
 
