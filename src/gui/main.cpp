846e85ce2e9638145c238f8820110ed09043497b
Johan Smet <johan.smet@justcode.be>
2021-10-11 21:18:27 +0200

    Remove SDL_INIT_GAMECONTROLLER flag from SDL_Init call.
    It causes issues in an http-connection on some browsers (firefox).
    And we don't use it anyway.

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char**argv)
     }
 
     // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
     {
         printf("Error: %s\n", SDL_GetError());
         return -1;
